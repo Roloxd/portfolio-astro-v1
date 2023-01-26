41ffa5c4eee4cd310751067f5ff5a2cfc2617073
Javier <rol0@outlook.es>
2023-01-26 13:17:51 +0000

    deploy-1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,6 @@
-import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
-import react from "@astrojs/react";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  site: "https://javimm.github.io",
 });
