c80c21c899dfb10c9d1373ab8df2961828f88a43
Javier <rol0@outlook.es>
2023-01-26 13:35:16 +0000

    deploy final

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://<YOUR_USERNAME>.github.io",
+  site: "https://roloxd.github.io",
 });
